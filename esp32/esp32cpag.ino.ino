309fe37bdb09559a88b88a9cda9731391176a4b8
Gabriel Camilo <82406455+gcamilop@users.noreply.github.com>
2025-12-03 14:37:26 -0300

    Change static IP address from 192.168.1.12 to 192.168.1.120

--- esp32/esp32cpag.ino.ino
+++ esp32/esp32cpag.ino.ino
@@ -10,7 +10,7 @@ WebServer server(80);
 
 Servo myServo;
 
-IPAddress staticIP(192, 168, 1, 12);
+IPAddress staticIP(192, 168, 1, 120);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(192, 168, 1, 1);
